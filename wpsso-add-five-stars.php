748a25385b95c21e6b84e3cffaf1fe5ee567ec26
JS Morisset <jsm@surniaulula.com>
2022-01-20 09:10:18 -0800

    v1.2.0

--- wpsso-add-five-stars.php
+++ wpsso-add-five-stars.php
@@ -13,8 +13,8 @@
  * Description: Add a 5 star rating and review from the site organization if the Schema markup does not already have an 'aggregateRating' property.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.8.3
- * WC Tested Up To: 6.1.0
+ * Tested Up To: 5.9.0
+ * WC Tested Up To: 6.1.1
  * Version: 1.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
