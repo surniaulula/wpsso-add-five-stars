98f87cf9617039a7f6a7fd57ba07841a57836e8d
JS Morisset <jsm@surniaulula.com>
2023-01-25 10:17:52 -0800

    v1.3.1-dev.6

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoAfsConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoafs' => array(			// Plugin acronym.
-					'version'     => '1.3.1-dev.5',	// Plugin version.
+					'version'     => '1.3.1-dev.6',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO AFS',	// Short plugin name.
 					'name'        => 'WPSSO Add Five Stars',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoAfsConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '14.6.1-dev.5',
+							'min_version'   => '14.7.0-dev.6',
 						),
 					),
 
