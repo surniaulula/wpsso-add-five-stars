d3f833d2160d8f3cc869a5cec11be2208e3552d0
JS Morisset <jsm@surniaulula.com>
2023-04-26 06:29:58 -0700

    v1.3.1

--- wpsso-add-five-stars.php
+++ wpsso-add-five-stars.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Add a 5 star rating and review from the site organization if the Schema markup does not already have an 'aggregateRating' property.
  * Requires Plugins: wpsso
- * Requires PHP: 7.2
+ * Requires PHP: 7.2.5
  * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * Version: 1.3.1
