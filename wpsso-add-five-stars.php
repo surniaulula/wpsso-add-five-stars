f8b844ee8674e08a569083624aaf57bee890f532
jsmoriss <jsm@surniaulula.com>
2024-09-14 22:37:03 +0000

    v2.2.0

--- wpsso-add-five-stars.php
+++ wpsso-add-five-stars.php
@@ -12,8 +12,8 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Add a 5 star rating and review from the site organization if the Schema markup does not already have an 'aggregateRating' property.
  * Requires Plugins: wpsso
- * Requires PHP: 7.2.34
- * Requires At Least: 5.8
+ * Requires PHP: 7.4.33
+ * Requires At Least: 5.9
  * Tested Up To: 6.6.2
  * Version: 2.2.0
  *
